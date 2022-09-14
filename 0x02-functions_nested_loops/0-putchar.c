ed1c96713f11d47abb158d7bdbfc5d9702fccafd
oluwatomigbekele <oluwatomishittu@gmail.com>
2022-09-13 18:33:16 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,19 @@
 #include "main.h"
 #include "unistd.h"
 
-/*
+/**
 * main - prints _putchar message.
 *
 * Description: prints _putchar
 *
-* Return - Always 0 success
+* Return: 0 (success)
 */
 int main(void)
 {
 	char text[8] = "_putchar";
 	int ite = 0;
 
-	for (ite = 0 ; ite < 9 ; ite++)
+	for (ite = 0 ; ite < 8 ; ite++)
 	{
 		_putchar(text[ite]);
 	}
