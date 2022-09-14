ab9efa45a9ffa017275219efffe7b36afd30ea5d
oluwatomigbekele <oluwatomishittu@gmail.com>
2022-09-13 18:22:20 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,19 @@
 #include "main.h"
-#include "_putchar.c"
+#include "unistd.h"
 
 /*
 * main - prints _putchar
 *
 * Return - Always 0 success
 */
-void main(void)
+int  main(void)
 {
-	putchar('_putchar');
-	putchar('\n');
+	char text[8]="_putchar";
+	int ite=0;
+	for (ite=0;ite<9;ite++)
+	{
+		_putchar(text[ite]);
+	}
+	_putchar('\n');
 	return (0);
 }
