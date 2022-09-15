bcdd5c915d6499b52b197656c935d4eb6f7c48b5
oluwatomigbekele <oluwatomishittu@gmail.com>
2022-09-15 10:35:13 -0700

    Uppercase detector

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/*
+ * _isupper - checks if uppercase
+ * @c: character to check
+ *
+ * Return: 1 if true and 0 otherwise
+ */
 int _isupper(int c)
 {
 	if (c >= 65 && c <= 90)
