70de2026335f95ff82556141956c97f856d5d3d0
oluwatomigbekele <oluwatomishittu@gmail.com>
2022-09-15 16:57:00 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* print_numbers - function that prints numbers
+* Description: print numbers from 1 to 10
+* Return: mul.
+*/
+void print_numbers(void)
+{
+	int num;
+
+	for (num = 0 ; num < 10 ; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+}
