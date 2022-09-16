472f927c322d15bd08b5d2ccc05c6ec1e9ec1a27
oluwatomigbekele <oluwatomishittu@gmail.com>
2022-09-15 18:23:40 -0700

    print underscore

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* print_line - function that prints straight line
+* @n: integer passed
+*/
+void print_line(int n)
+{
+	if (n > 0)
+	{
+		while (n)
+		{
+			_putchar('95');
+		}
+	}
+	_putchar('\n');
+}
