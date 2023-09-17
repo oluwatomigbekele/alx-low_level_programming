e1da2e2af5baee16c04fa3c9b7c1860e5a29dbf8
oluwatomigbekele <shittuoluwatomi@gmail.com>
2023-09-17 01:31:40 -0700

    advanced task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main  - Entry to the function
+ * Return: Always (0) Success
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is");
+	fprintf(stderr, "useful\" - Dora Korpar, 2015-10-19\n");
+		return (1);
+}
